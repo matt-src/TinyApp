f9db7a7736a7db3b2ca4c7a47116e543786e4c3c
tofurocks <mattwall0@gmail.com>
2018-09-09 18:41:29 -0700

    finished major requirements, all key functionality should be working (register/login, make/edit/delete/goto URLS, hashing/encryption)

---
+++ README.md
@@ -0,0 +1,2 @@
+TinyApp
+Allows users to create accounts where they can generate and store shortened forms of links
--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 var app = express();
-app.use(cookieParser());
+//app.use(cookieParser());
 var PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
-
 let templateVars = {
 	user: 'none',
 	users: {}
@@ -17,13 +17,13 @@ var hashedPassword = undefined;
 
 app.use(cookieSession({
   name: 'session',
-  keys: ['key1', 'key2']
+  keys: ['key123', 'key142']
 }))
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
-	templateVars.user = users[req.cookies.user_id];
+	templateVars.user = users[req.session.user_id];
 	templateVars.users = users;
 	next();
 });
@@ -54,7 +54,7 @@ const users = {
 
 app.get('/', (req, res) => {
 	let templateVars = { 
-		user: req.cookies['user_id'],
+		user: req.session.user_id,
 		users: users
 	};
 	res.render('partials/_header', templateVars);
@@ -63,39 +63,37 @@ app.get('/', (req, res) => {
 app.get('/urls.json', (req, res) => {
 	res.json(urlDatabase);
 });
-
-app.get('/hello', (req, res) => {
-	res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
+//Retrieve URLs if we are logged in
 app.get('/urls', (req, res) => {
 	let templateVars = { 
-		user: req.cookies['user_id'],
+		user: req.session.user_id,
 		urls: urlDatabase,
 		users: users
   };
   if(!templateVars.user){
-    res.send('Please log in to access this page')
+		res.send('Please log in to access this page <br /> \
+		<a href="/login">Log In</a> <br /> \
+		<a href="/register">Register</a>'
+		)
   }
 	res.render('urls_index', templateVars);
 });
-
+//Serve the create new URL page
 app.get('/urls/new', (req, res) => {
 	let templateVars = { 
-		user: req.cookies['user_id'],
+		user: req.session.user_id,
 		urls: urlDatabase,
 		users: users
 	};
-	if(!req.cookies['user_id']){
-		console.log('Trying to create new URL but we\'re not logged in');
+	if(!req.session.user_id){
 		res.redirect('/login');
 	}
 	res.render('urls_new', templateVars);
 });
-
+//Show URL with a specific short ID
 app.get('/urls/:id', (req, res) => {
 	let templateVars = { 
-		user: req.cookies['user_id'],
+		user: req.session.user_id,
 		shortURL: req.params.id,
 		urls: urlDatabase,
 		users: users
@@ -104,42 +102,43 @@ app.get('/urls/:id', (req, res) => {
 });
 //Receive new url and add it 
 app.post('/urls', (req, res) => {
-	console.log(req.body);  // debug statement to see POST parameters
 	var tmp = {
-		userID : req.cookies['user_id'],
+		userID : req.session.user_id,
 		longURL : req.body.longURL
   };
   if(typeof userID == undefined){
     res.send('Please log in to access this page')
-  }
+	}
+	//Generate a random key for the new user and store their information under it
 	urlDatabase[generateRandomString(6)] = tmp;
 	res.redirect('/urls');         // Respond with 'Ok' (we will replace this)
-	console.log('new URL db is ' + JSON.stringify(urlDatabase));
 });
 //Receive a new url to update 
 app.post('/urls/:id', (req, res) => {
 	var shortURL = req.params.id;
   var newURL = req.body.newURL;
   var tmp = {
-		userID : req.cookies['user_id'],
+		userID : req.session.user_id,
 		longURL : newURL
   };
-	console.log('Updating URL with short-ID: ' + shortURL);  // debug statement to see POST parameters
-	console.log('new URL is: ' + newURL);
-	console.log('Old db: ' + JSON.stringify(urlDatabase));
 	urlDatabase[shortURL] = tmp;
-	console.log('New db: ' + JSON.stringify(urlDatabase));
 	res.redirect('/urls');
 });
-
+//Redirect to the LongURL associated with the shortURL specified
 app.get('/u/:shortURL', (req, res) => {
-	let longURL = urlDatabase[req.params.shortURL];
-	res.redirect(longURL);
+	let shortURL = req.params.shortURL
+	if(urlDatabase[shortURL].longURL){
+	var longURL = urlDatabase[shortURL].longURL;
+	}
+	else{
+		res.send("URL does not exist")
+	}
+	res.redirect('http://' + longURL);
 });
 
 app.get('/login', (req, res) => {
 	let templateVars = { 
-		user: req.cookies['user_id'],
+		user: req.session.user_id,
 		users: users,
 		shortURL: req.params.id,
 		urls: urlDatabase
@@ -150,13 +149,16 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
 	res.render('urls_register', templateVars);
 });
-
+//Register a new user if they entered a valid email + password, and email is not already in use
 app.post('/register', function (req, res) {
-	console.log('old user list: ' + JSON.stringify(users));
 	let newEmail = req.body.email;
+	for(var member in users){
+		if(users[member].email == newEmail){
+			res.send('Email already in use')
+		}
+	}
   let newPassword = req.body.password;
   const hashedPassword = bcrypt.hashSync(newPassword, 6);
-  console.log(hashedPassword + " hashed is " + hashedPassword);
 	if(newEmail.length < 1 || hashedPassword.length < 1){
 		res.status(400);
 		res.send('Empty email or password');
@@ -167,41 +169,29 @@ app.post('/register', function (req, res) {
 		email: newEmail,
 		password: hashedPassword
 	};
-	//set cookie
-	res.cookie('user_id', randID);
+	//set encrypted cookie
+  req.session.user_id = randID
+	//res.cookie('user_id', randID);
 	users[randID] = newUser;
-	console.log('new user list: ' + JSON.stringify(users));
 	res.redirect('/urls');
 });
-  
-// POST method route
-app.post('/urls/:id/delete', function (req, res) {
+	//Delete URL with specified ID if requester is the owner
+ app.post('/urls/:id/delete', function (req, res) {
   var delTarg = req.body.delTarg;
   if(typeof urlDatabase[delTarg] == undefined){
     res.send('It appears our database has broken, please reload the page')
   }
 	let ownerID = urlDatabase[delTarg].userID;
-  let userID = req.cookies['user_id'];
-  console.log('Received delete request ' + delTarg);
-	console.log('owner is ' + ownerID);
-	console.log('our id is ' + userID);
-	if(!req.cookies['user_id']){
-		console.log('Trying to create delete link but we\'re not logged in');
+  let userID = req.session.user_id;
+	if(!req.session.user_id){
 		res.redirect('/login');
 	}
 	else if (ownerID == userID){
-    console.log('We own this link! Let\'s delete it');
-    console.log('We are going to delete ' + JSON.stringify(urlDatabase[delTarg]))
     delete urlDatabase[delTarg]
 		res.redirect('/urls');
 	}
 	else {
-		console.log('houston we have a problem');
-		/*console.log("Old db: " + JSON.stringify(urlDatabase))
-       console.log("We are going to delete " + urlDatabase[delTarg])
-       delete urlDatabase[delTarg]
-       console.log("New db: " + JSON.stringify(urlDatabase))
-       res.redirect("/urls")*/
+		res.send('something went wrong')
 	}
 });
 
@@ -210,50 +200,38 @@ app.post('/login', (req, res) => {
 	let email = req.body.email;
 	let password = req.body.password;
 	var found = false;
-	console.log('trying to log in with email ' + email  + ' and password ' + password);
 	//See if the email exists
-	for(var em in users){
-		console.log(em);
-        
-		console.log('checking ' + users[em].email);
-		if(users[em].email == email){
-			console.log('email: ' + email + ' located at id ' + em);
+	for(var member in users){
+		if(users[member].email == email){
 			found = true;
 		}
-		//res.status(403);
-		//res.send('Email not found');
 	}
 	if(!found){
 		res.status(403);
 		res.send('Email not found');
   }
 	else{
-    let targetHash = users[em].password;
-    console.log('Email found, trying password');
-    console.log("Our password is " + password + ", hashed password is " + targetHash);
+		//See if our the hash of our password matches the hash stored in our DB for the user
+    let targetHash = users[member].password;
 		let authed = false;
     authed = bcrypt.compareSync(password, targetHash);
-    console.log("Results of comparison is " + authed)
       if(authed){
-			console.log('Password is correct! Logged in!');
-			res.cookie('user_id', em);
+			req.session.user_id = member
       res.redirect('/');
       }
       else{
-        console.log('Password is incorrect!');
         res.redirect('/');
       }
 		}
 	});
 
 app.post('/logout', (req, res) => {
-	console.log('Logging out');
-	res.clearCookie('user_id');
+	res.clearCookie('session');
 	res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
-	console.log(`Example app listening on port ${PORT}!`);
+	//Listening
 });
 
 function generateRandomString(digits) {
@@ -267,5 +245,3 @@ function generateRandomString(digits) {
 
 	return text;
 }
-
-console.log(generateRandomString(6));
